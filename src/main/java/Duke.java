7f0c8fab85bc75e71a0e1ab95ebe0b62b9424bb9
tanqiuyu.a0177803y <qiuyu.87@gmail.com>
2020-08-26 01:12:02 +0800

    Enhanced version of Level-2 functionalities.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,36 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String dukeDivider = "\t----------------------------------------------------------------------------";
+        Date now = new Date();
+        SimpleDateFormat dateToday = new SimpleDateFormat("dd MMMM yyyy");
+        SimpleDateFormat dayToday = new SimpleDateFormat("EEEE");
 
-        System.out.println("\t--------------------------");
-        System.out.println("\tHello! I'm Duke");
-        System.out.println("\tWhat can I do for you?");
-        System.out.println("\t--------------------------");
+        String logo =
+                "====================================================================================\n" +
+                "====================================================================================\n" +
+                "===  DDDDDDDDD     UUU      UUU  KKK      KKK  EEEEEEEEEEEE            A0177803Y ===\n" +
+                "===  DDDDDDDDDD    UUU      UUU  KKK     KKK   EEEEEEEEEEE                       ===\n" +
+                "===  DDD     DDD   UUU      UUU  KKK    KKK    EEE                               ===\n" +
+                "===  DDD      DDD  UUU      UUU  KKK   KKK     EEEEEEEEEE                        ===\n" +
+                "===  DDD      DDD  UUU      UUU  KKK  KKK      EEEEEEEEEE                        ===\n" +
+                "===  DDD      DDD  UUU      UUU  KKKKKKK       EEE                               ===\n" +
+                "===  DDD      DDD  UUU      UUU  KKKKKK        EEE           NOTEPAD,            ===\n" +
+                "===  DDD     DDD   UUU      UUU  KKK KKKK      EEE           TASK MANAGER,       ===\n" +
+                "===  DDDDDDDDDD     UUUUUUUUUU   KKK   KKKK    EEEEEEEEEEE   BUDGET ASSISTANT,   ===\n" +
+                "===  DDDDDDDDD       UUUUUUUU    KKK     KKKK  EEEEEEEEEEEE  AND MORE...!        ===\n" +
+                "====================================================================================\n" +
+                "====================================================================================\n";
+        System.out.print("\nWelcome to PROJECT >>>\n" + logo);
+        System.out.print(String.format("%1$-42s%2$42s", dateToday.format(now), dayToday.format(now)) + "\n\n");
+
+        System.out.println(dukeDivider);
+        System.out.println("\tHello! I'm Duke, your all-rounded personal assistant!");
+        System.out.println("\tWhat do you need done today?");
+        System.out.println(dukeDivider);
 
         String[] tasks = new String[100];
         int taskCount = 0;
@@ -22,20 +41,20 @@ public class Duke {
             input = in.nextLine();
 
             if(input.equals("bye")) {
-                System.out.println("\t--------------------------");
+                System.out.println(dukeDivider);
                 System.out.println("\tBye. Hope to see you again soon!");
-                System.out.println("\t--------------------------");
+                System.out.println(dukeDivider);
                 break;
             } else if(input.equals("list")) {
                 for(int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ": " + tasks[i].toString());
                 }
             } else {
-                System.out.println("\t--------------------------");
+                System.out.println(dukeDivider);
                 tasks[taskCount] = input;
                 System.out.println("\tadded: " + tasks[taskCount]);
                 taskCount++;
-                System.out.println("\t--------------------------");
+                System.out.println(dukeDivider);
             }
         }
     }
