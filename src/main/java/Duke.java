56504f9c98a5d4f969b6d674a3d452e65f64c402
tanqiuyu.a0177803y <qiuyu.87@gmail.com>
2020-08-25 21:38:08 +0800

    This is Level-2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,6 +13,9 @@ public class Duke {
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t--------------------------");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (true) {
             String input;
             Scanner in = new Scanner(System.in);
@@ -23,9 +26,15 @@ public class Duke {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t--------------------------");
                 break;
+            } else if(input.equals("list")) {
+                for(int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ": " + tasks[i].toString());
+                }
             } else {
                 System.out.println("\t--------------------------");
-                System.out.println("\t" + input);
+                tasks[taskCount] = input;
+                System.out.println("\tadded: " + tasks[taskCount]);
+                taskCount++;
                 System.out.println("\t--------------------------");
             }
         }
