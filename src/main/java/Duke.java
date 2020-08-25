322f8ea96d7bcc922d7f0cb9deb25faed79a7a87
erictongg <erictong305@gmail.com>
2020-08-25 20:30:28 +0800

    added level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printUserInput(){
+        String user_input;
+        Scanner input = new Scanner(System.in);
+        user_input = input.nextLine();
+        if(user_input.equals("bye")){
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\tBye. Hope to see you again soon!");
+            System.out.println("\t____________________________________________________________");
+            System.exit(0);
+        }
+        else{
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + user_input);
+            System.out.println("\t____________________________________________________________");
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +26,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        while(true){
+            printUserInput();
+        }
     }
 }
