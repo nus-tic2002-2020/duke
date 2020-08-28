fccdd3314a43a376954ae888a5636a041e8483fe
aiqianchee <cheeaq@u.nus.edu>
2020-08-29 01:01:41 +0800

    commit for level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,6 +12,24 @@ public class Duke {
         ifBye();
     }
 
+    private static String[] taskList = new String[100];
+    private static int numberOfTask = 0;
+
+    //add
+    public static void setTaskList(String input){
+        taskList[numberOfTask] = input;
+        numberOfTask++;
+    }
+
+    //list
+    public static void getTaskList(){
+        System.out.println("\t____________________________________________________________");
+        for (int i = 0; i< numberOfTask; i++){
+            System.out.println("\t" + (i+1) + ". " + taskList[i]);
+        }
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void greeting(){
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Duke");
@@ -21,20 +39,26 @@ public class Duke {
 
     public static void ifBye(){
         Scanner getInput = new Scanner(System.in);
-        String input = getInput.next();
+        String input = getInput.nextLine();
+        //next() place cursor within same line after reading input. nextLine() reads input including space between the words till\n
 
         while (input != null) {
-            if (input.equals("bye")){
+            if (input.equals("bye")) {
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                getTaskList();
+                break;
             }
-            //else just parrot
+            //none of the above
+            setTaskList(input);
             System.out.println("\t____________________________________________________________");
-            System.out.println("\t" + input);
+            System.out.print("\tadded: ");
+            System.out.println(input);
             System.out.println("\t____________________________________________________________");
-            input = getInput.next();
+            input = getInput.nextLine();
         }
     }
 }
