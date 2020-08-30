0fea0a20df9f0fcf68ed6c665df52ebbed0eea5c
Aloysius <aloysiuswwc@gmail.com>
2020-08-30 19:44:02 +0800

    Add & List functions added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void add(String[] store, String input, int index) {
+        store[index] = input;
+        System.out.println("Added: " + input);
+    }
+
+    public static void list(String[] store) {
+        int index = 1;
+        for (String i : store) {
+            if (i == null) {
+                return;
+            }
+            System.out.println(index + ". " + i);
+            index ++;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " _       ___     ____       _____      \n"
                 + "| |     |_ _|   | ___|     /  _  \\        \n"
@@ -10,16 +26,22 @@ public class Duke {
                 + "|____| |_____|  |____| /__/       \\__\\   \n";
 
         String input = "blank";
+        String[] store = new String[100];
+        int index = 0;
         Scanner in = new Scanner(System.in);
         System.out.println(logo + "\nHello, Lisa here~\nHow can I help you today?");
 
         while (input.equalsIgnoreCase("Bye") == false) {
             input = in.nextLine();
-            if (input.equalsIgnoreCase("Bye") == false) {
-                System.out.println(input);
+            if (input.equalsIgnoreCase("Bye") == true) {
+                System.out.println("Goodbye~ Hope to see you again soon! :3");
+            }
+            else if (input.equalsIgnoreCase("List") == true) {
+                list(store);
             }
             else {
-                System.out.println("Goodbye~ Hope to see you again soon! :3");
+                add(store, input, index);
+                index ++;
             }
         }
     }
