dffac4c03b6c8c93e4dbcb4292dd7293d1204e30
Aloysius <aloysiuswwc@gmail.com>
2020-08-30 16:28:44 +0800

    Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _       ___     ____       _____      \n"
+                + "| |     |_ _|   | ___|     /  _  \\        \n"
+                + "| |      | |    | |__     /  /_\\  \\      \n"
+                + "| |      | |    |___ |   /  ____   \\      \n"
+                + "| |__   _| |_    __| |  /  /     \\  \\    \n"
+                + "|____| |_____|  |____| /__/       \\__\\   \n";
+
+        String input = "blank";
+        Scanner in = new Scanner(System.in);
+        System.out.println(logo + "\nHello, Lisa here~\nHow can I help you today?");
+
+        while (input.equalsIgnoreCase("Bye") == false) {
+            input = in.nextLine();
+            if (input.equalsIgnoreCase("Bye") == false) {
+                System.out.println(input);
+            }
+            else {
+                System.out.println("Goodbye~ Hope to see you again soon! :3");
+            }
+        }
     }
 }
