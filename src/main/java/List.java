7e822b007b88fb857e12ce30966dc5e8622c3276
erictongg <erictong305@gmail.com>
2020-09-01 15:19:53 +0800

    added level 4

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -4,46 +4,84 @@ public class List{
     //variable
     private String[] list;
     private boolean[] done;
+    private char[] task_cat;
     private static int item_count = 0;
-    //private char[] task_cat; //for level 4
 
     //constructor
     public List(){
         this.list = new String[100];
         this.done = new boolean[100];
+        this.task_cat = new char[100];
     }
 
     //getters
-    private void printList(){
-        if(list[0] == null){
-            //System.out.println("List is empty");
-            duke_echo("List is empty");
-            return;
-        }//if end
+    public void duke_help(){
         System.out.println("\t____________________________________________________________");
-        System.out.println("\tHere are the tasks in your list:");
-        for(int i = 1 ; i <= list.length	; i++){
-            String tick = (this.done[i-1] == true) ? "✓" : "✗";
-            System.out.println("\t" + i + "." + "[" + tick + "] " +this.list[i-1]);
-            if(this.list[i] == null){
-                System.out.println("\t____________________________________________________________");
-                return;
-            }//end if
-        }//end for loop
+        System.out.println("\t" + "You have enter an invalid command!");
+        System.out.println("\t" + "Following are valid commands: ");
+        System.out.println(
+                "\t\tlist - Display tasks in list\n" +
+                        "\t\tdone <task number> - Mark task as completed\n" +
+                        "\t\ttodo <task description> - Add to do task without any date/time \n" +
+                        "\t\tdeadline <task description>/by <date/time> - Add task that need to be done before a specific date/time\n" +
+                        "\t\tevent <task description>/at <date/time> - Add tasks that start at a specific time and ends at a specific time\n"+
+                        "\t\tbye - End duke"
+        );
+        System.out.println("\t____________________________________________________________");
     }
-
-    //setters
     private void duke_echo(String message){
         System.out.println("\t____________________________________________________________");
         System.out.println("\t" + message);
         System.out.println("\t____________________________________________________________");
     }
+    private void printList(){
+        if(list[0] == null){
+            duke_echo("List is empty");
+            return;
+        }//if end
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHere are the tasks in your list:");
+        for(int i = 1 ; i <= item_count ; i++){
+            String tick = (this.done[i - 1] == true) ? "✓" : "✗";
+            String to_print = "[" + this.task_cat[i - 1] + "]" + "[" + tick + "] " + this.list[i - 1];
+            System.out.println("\t" + i + ". " + to_print);
+        }
+        System.out.println("\t____________________________________________________________");
+    }
+    private void print_task(int index){
+        String tick = (this.done[index] == true) ? "✓" : "✗";
+        String to_print = "\t[" + this.task_cat[index] + "]" + "[" + tick + "] " + this.list[index];
+        System.out.println(to_print);
+    }
+
+    //setters
     private void insert_item(String input){
-        this.list[item_count] = input;
+        String[] task_split = input.split(" ", 2);
+        switch(task_split[0]){
+            case "todo":
+                this.task_cat[item_count] = 'T';
+                this.list[item_count] = task_split[1];
+                break;
+            case "deadline":
+                this.task_cat[item_count] = 'D';
+                task_split = task_split[1].split("/", 2);
+                this.list[item_count] = task_split[0] + "(" + task_split[1] + ")";
+                break;
+            case "event":
+                this.task_cat[item_count] = 'E';
+                task_split = task_split[1].split("/", 2);
+                this.list[item_count] = task_split[0] + "(" + task_split[1] + ")";
+                break;
+        }//end switch
         this.done[item_count] = false;
         item_count++;
+        System.out.println("\t____________________________________________________________");
+        System.out.print("\tGot it. I've added this task:\n\t");
+        print_task(item_count - 1);
+        System.out.println("\tNow you have " + item_count + " tasks in the list.");
+        System.out.println("\t____________________________________________________________");
     }
-    public void mark_completion(String done_message){
+    private void mark_completion(String done_message){
         /*Remove any unwanted characters that are not int*/
         String number = done_message.substring(done_message.indexOf(" ") + 1);
         for(int i = 0 ; i < number.length() ; i++){
@@ -64,14 +102,20 @@ public class List{
         else{
             this.done[index - 1] = true;
             String tick = (this.done[index-1] == true) ? "✓" : "✗";
-            duke_echo("Nice! I've marked this task as done:\n\t[" + tick + "] " + this.list[index - 1]);
+            duke_echo("Nice! I've marked this task as done:\n\t[" + this.task_cat[index - 1] + "]" + "[" + tick + "] " + this.list[index - 1]);
         }
+    }
+    private void mark_all_complete(){
+        for(int i = 0 ; i < item_count ; i++){
+            this.done[i] = true;
+        }
+        duke_echo("All tasks(" + item_count + ") are marked as done");
     }
     public void reader(){
         String user_input;
         Scanner input = new Scanner(System.in);
         user_input = input.nextLine();
-        responses(user_input);
+        responses(user_input.trim());
     }
     private void responses(String user_input){
         if(user_input.equals("bye")){ //bye: end duke
@@ -81,12 +125,24 @@ public class List{
         else if(user_input.equals("list")){ //list: list items
             this.printList();
         }//end else if
-        else if(user_input.contains("done")){ //done: change done to true from false
+        else if(user_input.contains("done") && (user_input.equals("done") == false)){ //done: change done to true from false
+            if(item_count == 0){
+                duke_echo("List is empty!");
+                return;
+            }
+            if(user_input.equals("done all")){
+                this.mark_all_complete();
+                return;
+            }
             this.mark_completion(user_input);
         }
-        else{ //insertion of item into list
-            duke_echo("added: " + user_input);
+        else if((user_input.contains("todo") || user_input.contains("deadline") || user_input.contains("event")) &&
+                ((user_input.equals("todo") || user_input.equals("deadline") || user_input.equals("event")) == false)
+        ){ //insertion of item into list
             this.insert_item(user_input);
+        }//end else if
+        else{
+            duke_help();
         }//end else
     }
 }
