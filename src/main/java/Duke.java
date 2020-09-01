5cb9821ba115e01466ccb7a6f861489208953116
LAPTOP-USR9OL4K\zarif <zarifahbz@gmail.com>
2020-09-01 20:53:10 +0800

    Commit Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void greet() {
+        System.out.print("Hello! I'm Duke\n" + "What can I do for you?\n");
+    }
+
+    public static void printLine() {
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void echo() {
+        Scanner in = new Scanner(System.in);
+        String line;
+        line = in.nextLine();
+        printLine();
+        String endconvo = "bye";
+
+        if (line.equals(endconvo)) {
+            bye();
+            printLine();
+            return;
+
+        } else {
+            System.out.println(line);
+            printLine();
+            echo();
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +43,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        //System.out.print("Hello! I'm Duke\n" + "What can I do for you?\n");
+
+        greet();
+        printLine();
+        echo();
+
     }
 }
