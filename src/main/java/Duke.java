2db454d3959c4280e91207b2eb283cb2abbdd89e
Roshan Kumar <e00259951@u.nus.edu>
2020-09-02 14:30:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        if (line.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else {
+            System.out.println(line + "\n");
+            echo();
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        System.out.println(greet);
+        echo();
     }
 }
