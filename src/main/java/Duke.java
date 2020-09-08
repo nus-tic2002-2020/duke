b0d6e2cb78fe63e9ec5f9f8851f0421e0f6a0ea6
roshan.kumar@outlook.sg <roshan.kumar@outlook.sg>
2020-09-08 17:13:46 +0800

    Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,75 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Duke {
     public static int count = 0;
-    public static int seq = 1;
+    public static boolean trigger = true;
     public static ArrayList<Task> list = new ArrayList<>();
 
+    private static void writeToFile(String filePath) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        for (Task l : list) {
+            fw.write(l + System.lineSeparator());
+        }
+        fw.close();
+    }
+
+    private static void printFileContents(String filePath) throws DukeException , IOException {
+        File f = new File(filePath); // create a File for the given file path
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        if (!s.hasNext()) {
+            System.out.println("No existing data is found");
+        }
+        while (s.hasNext()) {
+            String current = s.nextLine();
+            System.out.println(current);
+            if (current.contains("[T]")) {
+                String description = current.substring(7);
+                list.add(new Todo(description));
+                if (current.contains("\u2713")) {
+                    list.get(count).setStatus(true);
+                }
+            }
+            else if (current.contains("[D]")) {
+                int m = current.indexOf("(");
+                int n = current.indexOf(")");
+                String description = current.substring(7,m-1);
+                String by = current.substring(m+5,n);
+                list.add(new Deadline(description,by));
+                if (current.contains("\u2713")) {
+                    list.get(count).setStatus(true);
+                }
+            }
+            else if (current.contains("[E]")) {
+                int m = current.indexOf("(");
+                int n = current.indexOf(")");
+                String description = current.substring(7,m-1);
+                String at = current.substring(m+5,n);
+                list.add(new Event(description,at));
+                if (current.contains("\u2713")) {
+                    list.get(count).setStatus(true);
+                }
+            }
+            else {
+                throw new DukeException("Error: Task in existing data is incompatible\n");
+            }
+            count++;
+        }
+    }
+
     public static void sort(String t) throws DukeException { //classify tasks between todo / deadline / event
-        if (t.contains("todo")) {
+        if (t.toLowerCase().contains("todo")) {
             if (t.trim().length() < 5) {
                 throw new DukeException("Error: Description of task cannot be empty.\n");
             }
-            String description = t.substring(5);
+            int m = t.toLowerCase().indexOf("todo");
+            String description = t.substring(m+4).trim();
             for (Task l : list) {
                 if (l.description.equals(description)) {
                     throw new DukeException("Error: Task has already been added previously\n");
@@ -20,16 +77,17 @@ public class Duke {
             }
             list.add(new Todo(description));
         }
-        else if (t.contains("deadline")) {
+        else if (t.toLowerCase().contains("deadline")) {
             if (t.trim().length() < 9) {
                 throw new DukeException("Error: Description of task cannot be empty.\n");
             }
             if (!t.contains("/")) {
                 throw new DukeException("Error: Please specify time.\n");
             }
+            int m = t.toLowerCase().indexOf("deadline");
             int n = t.indexOf('/');
-            String description = t.substring(9, n-1);
-            String by = t.substring(n+4);
+            String description = t.substring(m+8,n).trim();
+            String by = t.substring(n+3).trim();
             for (Task l : list) {
                 if (l.description.equals(description)) {
                     throw new DukeException("Error: Task has already been added previously\n");
@@ -37,16 +95,17 @@ public class Duke {
             }
             list.add(new Deadline(description, by));
         }
-        else if (t.contains("event")) {
+        else if (t.toLowerCase().contains("event")) {
             if (t.trim().length() < 6) {
                 throw new DukeException("Error: Description of task cannot be empty.\n");
             }
             if (!t.contains("/")) {
                 throw new DukeException("Error: Please specify time.\n");
             }
+            int m = t.toLowerCase().indexOf("event");
             int n = t.indexOf('/');
-            String description = t.substring(6, n-1);
-            String at = t.substring(n+4);
+            String description = t.substring(m+5,n).trim();
+            String at = t.substring(n+3).trim();
             for (Task l : list) {
                 if (l.description.equals(description)) {
                     throw new DukeException("Error: Task has already been added previously\n");
@@ -59,30 +118,33 @@ public class Duke {
         }
     }
 
-    public static void echo() throws DukeException{ //classify commands such as bye / done / list / add tasks
-        String line;
+    public static void echo() throws DukeException{ //classify commands such as bye / done / list / add various tasks
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
+        String line = in.nextLine();
 
-        if (line.equals("bye")) {
+        if (line.trim().equalsIgnoreCase("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
             System.exit(0);
         }
-        else if (line.equals("list")) {
+        else if (line.trim().equalsIgnoreCase("list")) {
+            if (count == 0) {
+                throw new DukeException("There are no items currently in the list\n");
+            }
             System.out.println("Here are the tasks in your list:\n");
+            int seq = 1;
             for (Task l : list) {
                 System.out.println(seq + ". " + l);
                 seq++;
             }
-            System.out.println("\n");
-            seq = 1;
+            trigger = false;
         }
-        else if (line.contains("done")) {
-            String[] words = line.split(" ");
-            if (words.length < 2 || words[1].trim().equals("")) {
+        else if (line.toLowerCase().contains("done")) {
+            int m = line.toLowerCase().indexOf("done");
+            String num = line.substring(m+4).trim();
+            if (num.length() < 1) {
                 throw new DukeException("Error: Please enter which task is done\n");
             }
-            int n = Integer.parseInt(words[1]);
+            int n = Integer.parseInt(num);
             if (n > count) {
                 throw new DukeException("Error: Please enter a valid task number\n");
             }
@@ -95,12 +157,13 @@ public class Duke {
                 System.out.println(list.get(n-1) + "\n");
             }
         }
-        else if (line.contains("delete")) {
-            String[] words = line.split(" ");
-            if (words.length < 2 || words[1].trim().equals("")) {
+        else if (line.toLowerCase().contains("delete")) {
+            int m = line.toLowerCase().indexOf("delete");
+            String num = line.substring(m+6).trim();
+            if (num.length() < 1) {
                 throw new DukeException("Error: Please enter which task to be deleted\n");
             }
-            int n = Integer.parseInt(words[1]);
+            int n = Integer.parseInt(num);
             if (n > count) {
                 throw new DukeException("Error: Please enter a valid task number\n");
             }
@@ -113,7 +176,7 @@ public class Duke {
             }
         }
         else {
-            sort(line); //Move into sort method
+            sort(line); //add various tasks using sort method
             System.out.println("Got it. I've added this task:");
             System.out.println(list.get(count));
             count++;
@@ -121,14 +184,33 @@ public class Duke {
         }
     }
 
-    public static void main(String[] args) throws DukeException {
-        String greet = "Hello! I'm Duke\n" + "What can I do for you?\n";
-        System.out.println(greet);
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke\n" + "Let me load the existing data for you (if any)\n");
+        String FileLocation = "data/duke.txt";
+        String Directory = "./data/";
+        try {
+            Path path = Paths.get(Directory);
+            Files.createDirectories(path);
+            printFileContents(FileLocation);
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+            trigger = true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("What would you like to do ?");
         while(true) {
             try {
-                echo(); //push into recursive loop echo()
+                echo(); //push into loop echo()
+                if (trigger) {
+                    writeToFile(FileLocation); //only make changes when task list changes
+                }
+                trigger = true;
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
+                trigger = true;
+            } catch (IOException e) {
+                System.out.println("Something went wrong: " + e.getMessage());
             }
         }
     }
