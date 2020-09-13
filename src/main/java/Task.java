3ccf2cb1e001fedac67ad657136650f70425ac8d
Faiz Rashid <faiz.rashid@gmx.us>
2020-09-13 22:37:15 +0800

    Remove constructor

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,9 +7,17 @@ public class Task {
         this.isDone = false;
     }
 
+   // public Task() {
+     //   this.list = new String[100];
+       // this.done = new boolean[100];
+    //}
+
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
     //... do i need anything here?
+    public void setDone (boolean state){
+        this.isDone = state;
+    }
 }
