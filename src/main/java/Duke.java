ab7902bc2fd78b5adea9dcf6aa9e34c6b90fb2c9
mtahgg <mtahgg@outlook.com>
2020-09-14 22:05:12 +0800

    level1&2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        int wordCounter = 0;
+        String[] words = new String[100];
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello Again");
+        System.out.println(logo + "Hello! I'm Duke\n" + "What can I do for you?");
+
+        while(true){
+            Scanner input = new Scanner(System.in);
+            String userInput = input.nextLine();
+
+            if(userInput.equals("bye") || userInput.equals("Bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }else if(userInput.equals("list")){
+                for(int i = 0; i < wordCounter; i++){
+                    System.out.println((i + 1) + ". " + words[i]);
+                }
+                continue;
+            }
+
+            words[wordCounter] = userInput;
+            System.out.println("added: " + words[wordCounter]);
+            wordCounter++;
+        }
     }
 }
---
+++ src/main/java/dukeText.java
@@ -0,0 +1,8 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class dukeText {
+    Graphics g;
+    
+}
