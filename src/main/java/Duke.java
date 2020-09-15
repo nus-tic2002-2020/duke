a3ff5535abf379a040386dbb8fa428a79f187fc3
Donnus <donnusyu@gmail.com>
2020-09-15 20:58:42 +0800

    add Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            String echo = scan.nextLine();
+
+            if (echo.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                System.out.println(echo);
+            }
+        }
     }
 }
