5b20e3409428991ac65e7a77b82785f2fc0a6af0
SILVERMIST <e0374052@u.nus.edu>
2020-09-15 20:43:29 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;  //
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from silvermist\n" + logo+"\n oyoyoyoyooyyo");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+       chat();
     }
+    public static void chat() {
+    	Scanner userinput = new Scanner(System.in);  // Create a Scanner object
+        String value=(userinput.nextLine()).trim();
+        String ln="  ____________________________________________________________\n";
+        
+        if(value.equals("bye")) {
+        	System.out.println(ln+"   Bye. Hope to see you again soon!\n"+ln);  // Output user input
+        }else {
+        	System.out.println(ln+"  " +value+"\n"+ln);
+        	chat();
+        	// Output user input
+        }
+          }
 }
