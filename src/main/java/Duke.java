332daf8cf2b829817b686ec8e5dafd6349b923c5
darrenwzw <darrenfire955@gmail.com>
2020-09-16 17:19:21 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        int i=0;
+        String[] list = new String[100];
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,15 +13,26 @@ public class Duke {
 
         while(true) {
             Scanner input = new Scanner(System.in);  // Create a Scanner object
-            System.out.println("Please Enter Command: ");
+            System.out.println("What can I do for you?");
             String command = input.nextLine();  // Read user input
             if (command.equals("bye"))
             {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            else{
+            else if (command.equals("list"))
+            {
+                for(int z=0; z<i; z++)
+                {
+                    System.out.println(z+1+": "+list[z]);
+                }
+                //System.out.println(command);
+            }
+            else
+            {
+                list[i]=command;
                 System.out.println(command);
+                i++;
             }
         }
     }
