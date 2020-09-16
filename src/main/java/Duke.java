4c382ed8b33544d28b35e31960f0f8aee06277ca
Donnus <donnusyu@gmail.com>
2020-09-16 23:27:16 +0800

    add Level-2 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+
+        List<String> storeText = new ArrayList<String>();
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
 
@@ -9,9 +13,18 @@ public class Duke {
             Scanner scan = new Scanner(System.in);
             String echo = scan.nextLine();
 
+            if (!echo.equals("list")) {
+                storeText.add(echo);
+            }
+
             if (echo.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }
+            else if (echo.equals("list")) {
+                for (int i = 0; i < storeText.size(); i++) {
+                    System.out.println(i+1 + ". " + storeText.get(i));
+                }
             }
             else {
                 System.out.println(echo);
