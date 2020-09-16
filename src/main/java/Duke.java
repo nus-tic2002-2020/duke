e8960f1a3c6990b479fb83aa80caddda6c0b1e9c
darrenwzw <darrenfire955@gmail.com>
2020-09-16 17:07:14 +0800

    basic ui

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        while(true) {
+            Scanner input = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Please Enter Command: ");
+            String command = input.nextLine();  // Read user input
+            if (command.equals("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else{
+                System.out.println(command);
+            }
+        }
     }
 }
