3a0cdb910b81334750c9dfbc721e14437cfa3b7f
Ashikin A Ghani <e0374105@u.nus.edu>
2020-09-18 23:13:00 +0800

    Level 3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static String[] printUserInput(String[] listItems){
+    public static Task[] printUserInput(Task[] listItems){
         //user input
         String userInput;
         Scanner input = new Scanner(System.in);
         userInput = input.nextLine();
-        //type bye to exit
+        //bye to exit
         if (userInput.equals("bye")) {
             System.out.println("\t____________________________________________________________");
             System.out.println("\tBye. Hope to see you again soon!");
@@ -19,6 +19,11 @@ public class Duke {
         else if(userInput.equals ("list")) {
             printUserList(listItems);
         }
+        else if (userInput.startsWith ("done"))
+        {
+            doneMessage(listItems, userInput);
+
+        }
         else{
             System.out.println("\t____________________________________________________________");
             listItems = storeUserList (listItems, userInput);
@@ -28,11 +33,11 @@ public class Duke {
     }
 
     //store items in list
-    public static String[] storeUserList(String[] listItems, String input) {
+    public static Task[] storeUserList(Task[] listItems, String input) {
         int counter = 0;
             while(true){
                 if(listItems[counter] == null){
-                    listItems[counter] = input;
+                    listItems[counter] = new Task(input);
                     System.out.println ("\tadded: " + input);
                     break;
                 }
@@ -42,7 +47,7 @@ public class Duke {
     }
 
         //print items in list
-    public static void printUserList(String[] listItems){
+    public static void printUserList(Task[] listItems){
         if(listItems[0] == null){
             System.out.println("List is empty");
             return;
@@ -51,7 +56,7 @@ public class Duke {
         System.out.println("\t____________________________________________________________");
 
         for (int i = 1 ; i <= listItems.length ; i++){
-            System.out.println("\t" + i + ". " + listItems[i-1] );
+            System.out.println("\t" + i + ". " + "[" + listItems[i-1].getStatusIcon() + "]" + listItems[i-1].description);
                 if(listItems[i] == null){
                     System.out.println("\t____________________________________________________________");
                     return;
@@ -59,8 +64,25 @@ public class Duke {
             }
         }
 
+    public static Task[] doneMessage(Task[] listItems, String userInput){
+
+
+        int filteredNumber = Integer.parseInt(userInput.substring(5));
+        listItems[filteredNumber-1].markDone();
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t" + "[" + listItems[filteredNumber-1].getStatusIcon()+"]"+ listItems[filteredNumber-1].description);
+        System.out.println("\t____________________________________________________________");
+
+
+        return listItems;
+    }
+
     public static void main(String[] args) {
-        String [] ListofItems = new String[100];
+      //  String [] ListofItems = new String[100];
+        Task[] listTask = new Task[100];
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -72,7 +94,8 @@ public class Duke {
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
         while (true) {
-            ListofItems= printUserInput(ListofItems);
+            listTask= printUserInput(listTask);
+
         }
     }
 }
