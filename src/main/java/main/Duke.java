a25b51f00747f19468c49a8291a9ce4a48f6bd3a
Jayde-issexy <55940855+Jayde-issexy@users.noreply.github.com>
2020-09-20 07:44:01 +0800

    Level 2 - 20200920

--- src/main/java/main/Duke.java
+++ src/main/java/main/Duke.java
@@ -1,10 +1,13 @@
 package main;
 import java.util.Scanner;
+import java.util.ArrayList;
 //import parser.InputText;
 
-
 public class Duke {
+
     public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<String>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,11 +16,26 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
-
-        //InputText response = new InputText();
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        //response.parseInput(input);
-         System.out.println(input);
+        /*level 1*/
+        while (true) {
+            String input = in.nextLine();
+            if(!input.equals("list")){
+                list.add(input);
+            }
+            if(input.equals("list")) {
+                int i=0;
+                for(String name:list) {
+                    System.out.println(++i + ". " + name);
+                }
+            }
+            if(!input.equals("bye") && !input.equals("list")){
+                System.out.println("added: " + input);
+            }
+
+             if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+            }
+        }
     }
 }
--- src/main/java/parser/InputText.java
+++ src/main/java/parser/InputText.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class InputText {
 
-
+    /*
+     * input - User Input to the application
+     * @throws DukeException - catches Duke exceptions and returns a suitable message to the user
+     * @throws IOException
+     */
     public static void parseInput(String input) {
 
     }
