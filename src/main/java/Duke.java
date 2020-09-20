7e997837284bd390faa54d93bd6270e954f5692f
REALXZP <realxzp@gmail.com>
2020-09-20 20:47:03 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,19 +16,39 @@ public class Duke {
                      + "|  __/| |      { (_) }     \n"
                      + "|_|   |_|       `-^-'      \n";
 
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "I'm Pi,");
         System.out.println("What can I do for you?");
 
         String input;
         Scanner in = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int numberOfTask = 0;
+
         input = in.nextLine();
 
-        while(!input.equals("bye")){
+        while(!input.equals("bye")) {
             System.out.println(input);
+            if (input.equals("list")){
+                printList(tasks,numberOfTask);
+            }else{
+                tasks[numberOfTask] = input;
+                numberOfTask++;
+
+                System.out.println("added: " + input);
+            }
             input = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
 
+    }
+
+
+    /*print out the user input from the list*/
+    public static void printList(String[] tasks, int numberOfTask) {
+        for (int i = 0; i < numberOfTask; i++ ) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
     }
 
 
