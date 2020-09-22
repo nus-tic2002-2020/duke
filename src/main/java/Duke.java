9c3280fdc575c684fb5c13a6716715b9c8c101dc
LAPTOP-USR9OL4K\zarif <zarifahbz@gmail.com>
2020-09-22 18:27:47 +0800

    Level 6 : Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -18,7 +19,7 @@ public class Duke {
     public static void command() {
 
         String[] list = new String[10]; // create list!
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<Task>(); //change to Array
         int pointer = 0;
 
         Scanner in = new Scanner(System.in);
@@ -29,72 +30,85 @@ public class Duke {
 
         while (true) {
 
-            line = in.nextLine();
-            if (line.equals(endconvo)) { //if user enter bye
-                bye();
-                printLine();
-                return;
+            try {
 
-            } else if (line.equals("list")) {  //number of list
+                line = in.nextLine();
+                if (line.equals(endconvo)) { //if user enter bye
+                    bye();
+                    printLine();
+                    return;
 
-                System.out.println("   Here are the tasks in your lists: ");
-                for (int i = 0; i < pointer; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i].toString()); //shows tasklist
+                } else if (line.equals("list")) {  //number of list
 
-                }//printLine();
+                    System.out.println("   Here are the tasks in your lists: ");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + ". " + tasks.get(i).toString()); //shows tasklist
 
-            } else if (line.contains("done")) {
+                    }//printLine();
 
-                int j = Integer.parseInt(line.substring(5)) - 1; //refer to which list is done and mark it
-                Task currenttask = tasks[j];
-                currenttask.markDone();
-                System.out.println("   Nice! I've marked this task as done:");
-                System.out.println(currenttask.toString());
-                //printLine();
+                } else if (line.contains("done")) {
 
+                    int j = Integer.parseInt(line.substring(5)) - 1; //refer to which list is done and mark it
+                    Task currenttask = tasks.get(j);
+                    currenttask.markDone();
+                    System.out.println("   Nice! I've marked this task as done:\n " + currenttask.toString());
 
-            } else if (line.contains("todo")) { //try to combine all three classes 
+                } else if (line.contains("delete")) {
 
-                tasks[pointer] = new ToDo(line.substring(5));
+                    int delete = Integer.parseInt(line.substring(7)) - 1; //delete
+                    Task currenttask = tasks.get(delete);
+                    tasks.remove(delete);
 
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[pointer].toString());
-                System.out.println("Now you have " + (pointer + 1) + " tasks in the list.");
-                pointer++;
-                //System.out.println("added: " + line);
+                    System.out.println("   Noted. I've removed this task:\n " + currenttask.toString());
+                    System.out.println("   Now you have " + (tasks.size()) + " tasks in the list.");
+                    pointer++;
 
-            } else if (line.contains("event")) {
+                } else if (line.contains("todo")) { //try to combine all three classes
 
-                int separator = line.indexOf('/');
-                tasks[pointer] = new Event(line.substring(6, separator - 1), line.substring(separator + 4));
+                    tasks.add(new ToDo(line.substring(5)));
 
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[pointer].toString());
-                System.out.println("Now you have " + (pointer + 1) + " tasks in the list.");
-                pointer++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size() - 1).toString());
+                    System.out.println("Now you have " + (tasks.size()) + " tasks in the list.");
+                    pointer++;
+                    //System.out.println("added: " + line);
 
+                } else if (line.contains("event")) {
 
-            } else if (line.contains("deadline")) {
+                    int separator = line.indexOf('/');
+                    tasks.add(new Event(line.substring(6, separator - 1), line.substring(separator + 4)));
 
-                int separator = line.indexOf("/");
-                tasks[pointer] = new Deadline(line.substring(9, separator - 1), line.substring(separator + 4));
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[pointer].toString());
-                System.out.println("Now you have " + (pointer + 1) + " tasks in the list.");
-                pointer++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size() - 1).toString());
+                    System.out.println("Now you have " + (tasks.size()) + " tasks in the list.");
+                    pointer++;
 
 
-            } else {
+                } else if (line.contains("deadline")) {
 
-                System.out.println("   added: " + line); //add to the list
-                list[pointer] = line;
-                Task newtask = new Task(line);
-                newtask.description = line;
-                tasks[pointer] = newtask;
-                pointer++;
+                    int separator = line.indexOf("/");
+                    tasks.add(new Deadline(line.substring(9, separator - 1), line.substring(separator + 4)));
 
-                //  }printLine();
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks.get(tasks.size() - 1).toString());
+                    System.out.println("Now you have " + (tasks.size()) + " tasks in the list.");
+                    pointer++;
+
+
+                } else {
+
+                    System.out.println("   added: " + line); //add to the list
+                    Task newtask = new Task(line);
+                    newtask.description = line;
+                    //tasks.get(pointer) = newtask;
+                    pointer++;
+
+                }
+
+
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+
 
             }
         }
