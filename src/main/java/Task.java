2f9a07cfaad603cb3bf648363052ae7290eb4992
Faiz Rashid <faiz.rashid@gmx.us>
2020-09-26 21:46:53 +0800

    Level 5

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,7 +9,8 @@ public class Task {
 
 
 
-    public Task(String description) {
+    public Task(String description) throws IllegalInputException {
+
 
          this.isDone = false;
          Pattern pattern = Pattern.compile((".*" + "todo" + ".*"), Pattern.CASE_INSENSITIVE);
@@ -19,14 +20,24 @@ public class Task {
          Pattern pattern3 = Pattern.compile((".*" + "Event" + ".*"), Pattern.CASE_INSENSITIVE);
          Matcher matcher3 = pattern3.matcher(description);
          if (matcher.find()) {
+             //todo
             //implement a split
              String[] temp;
              temp = description.split("\\s",2);
+             if (temp.length == 1){
+                 System.out.println("Description cannot be empty");
+                 throw new IllegalInputException();
+             }
              this.description = temp[1];
              this.whatTag = "[T]";
              this.timeDate ="";
+             if (this.description.isBlank() == true){
+                 System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                 throw new IllegalInputException();
+             }
          }
         else if (matcher2.find()) {
+            //deadline
              String[] temp;
              temp = description.split("\\s",2);
              this.description = temp[1];
@@ -36,7 +47,7 @@ public class Task {
              this.timeDate=temp[1];
              this.timeDate="(by:"+this.timeDate+")";
          }
-         else if (matcher3.find()) {
+         else if (matcher3.find()) { //event
              String[] temp;
              temp = description.split("\\s",2);
              this.description = temp[1];
@@ -47,15 +58,11 @@ public class Task {
              this.timeDate="(at:"+this.timeDate+")";
          }
          else {
-
-             this.whatTag = "[undefined tag]";
+             System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+             throw new IllegalInputException();
          }
     }
 
-   // public Task() {
-     //   this.list = new String[100];
-       // this.done = new boolean[100];
-    //}
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
@@ -63,8 +70,5 @@ public class Task {
     public String getTag(){
         return (whatTag);
     }
-    //... do i need anything here?
-    public void setDone (boolean state){
-        this.isDone = state;
-    }
+
 }
