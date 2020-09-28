44ef67eedd13392a51a78dc42708dc0133e85e2e
Donnus <donnusyu@gmail.com>
2020-09-29 01:09:03 +0800

    add Level-3 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
 
-        List<String> storeText = new ArrayList<String>();
+        List<Task> taskList = new ArrayList<Task>();
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
 
@@ -13,17 +13,26 @@ public class Duke {
             Scanner scan = new Scanner(System.in);
             String echo = scan.nextLine();
 
-            if (!echo.equals("list")) {
-                storeText.add(echo);
+            if (!echo.equals("list") && !echo.contains("done")) {
+                Task newTask = new Task(echo);
+                taskList.add(newTask);
             }
 
-            if (echo.equals("bye")) {
+            if (echo.contains("done")) {
+                String doneSplit[] = echo.split(" ");
+                int item = Integer.parseInt(doneSplit[1]) - 1;
+                taskList.get(item).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + taskList.get(item).getStatusIcon() + "] " + taskList.get(item).toString());
+            }
+            else if (echo.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
             else if (echo.equals("list")) {
-                for (int i = 0; i < storeText.size(); i++) {
-                    System.out.println(i+1 + ". " + storeText.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(i+1 + "." + "[" + taskList.get(i).getStatusIcon() + "] " + taskList.get(i));
                 }
             }
             else {
