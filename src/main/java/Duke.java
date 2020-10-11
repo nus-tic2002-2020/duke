44b0d8d90eb63e01158e03cbee52d9b84d4f1884
DESKTOP-L8II7P1\Legen <factionsypho@gmail.com>
2020-10-11 13:12:04 +0800

    Completed level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,17 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-
 public class Duke {
-    //private static Task[] tasks = new Task[100];
+    private static Task[] t = new Task[100];
     private static int arrCounter = 0;
 
-
+    public static void setTask(Task description){
+        t[arrCounter] = description;
+        arrCounter++;
+    }
+    public static String markAsDone(int taskNumber){
+        return t[taskNumber].setDone();
+    }
     public static void commandInput(){
         int arrSize = 100;
         String[] commands = new String[arrSize];
@@ -15,29 +20,29 @@ public class Duke {
         String userCommand;
         do{
             userCommand = sc.nextLine();
+
             switch(userCommand){
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
                 case "print list":
                     for(int i = 0; i<arrCounter; i++){
-                        System.out.println(i+1 + ". " + commands[i]);
+                        System.out.println(i+1 + ".[" +t[i].getStatusIcon() + "] " +t[i].description);
                     }
                     break;
-                
                 default:
-                    commands[arrCounter] = userCommand;
-                    System.out.println("added: " + commands[arrCounter]);
-                    arrCounter++;
+                    if(userCommand.contains("mark")){
+                        System.out.println(markAsDone(Integer.parseInt(userCommand.split(" ")[1])-1));
+                        break;
+                    }
+                    setTask(new Task(userCommand));
+                    System.out.println("added: " + userCommand);
             }
 
         }while(!(userCommand.equals("bye")));
 
     }
-    /*
-    public static String markAsDone(int task){
-        return tasks[task].setDone();
-    }*/
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
