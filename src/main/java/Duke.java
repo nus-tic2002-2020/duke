3cb4d5ad0a1861b367f91bd9218bf38e9e400754
Natalie Yeo <>
2020-10-26 12:22:42 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
+    private static Task [] store = new Task [100];
+    private static int count =0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,26 +13,59 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        String [] store = new String [100];
-        String line;
-        int i =0;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        while (!line.equals("bye")) {
-            store[i] = line;
 
-
-            if (line.equals("list")){
-                for (int a = 0; a<i; a++){
-                    System.out.println(a+1 + ". " + store[a]);
-                }
-            }
-            i++;
-            if (!line.equals("list")) {
-                System.out.println("added: " + line);
-            }
-            line = in.nextLine();
+        int flag = 0;
+        while (flag == 0) {
+            String line = in.nextLine();
+            flag = text(line.trim());
         }
-        System.out.print("Bye. Hope to see you again soon!");
+    }
+
+    public  static int text(String input) {
+        String[] num = input.split(" ");
+        if (num.length > 0 && !num[0].isBlank()) {
+            String command = num[0];
+            switch (command) {
+                case "list":
+                    listTask();
+                    break;
+                case "done":
+                    Taskdone(num);
+                    break;
+                case "bye":
+                    System.out.print("Bye. Hope to see you again soon!");
+                    return 1;
+                default:
+                    addTask(input);
+            }
+        }
+        return 0;
+    }
+
+    public static void addTask (String description) {
+        Task t = new Task(description);
+        store[count++] = t;
+        System.out.println("added: " + t);
+    }
+
+    public static void listTask() {
+        System.out.println("Here are the tasks in your list: ");
+        for (int a = 0; a < count; a++) {
+            Task t = store[a];
+            System.out.println(a + 1 + ". " + "[" + store[a].getStatusIcon() + "] " + store[a]);
+        }
+    }
+
+    public static void Taskdone(String[] num){
+        System.out.println("Nice! I've marked this task as done: ");
+        for (int i = 1; i < num.length; i++){ //skip 0 as text, looking for the number
+            int TaskNo = Integer.parseInt(num[i]); //change string to No
+            if (store[TaskNo-1] != null ){ //to check that input is in list
+                Task t = store[TaskNo-1];
+                t.markAsDone();
+                System.out.println("[" + store[TaskNo-1].getStatusIcon() + "] "+ store[TaskNo-1]);
+            }
+        }
     }
 }
