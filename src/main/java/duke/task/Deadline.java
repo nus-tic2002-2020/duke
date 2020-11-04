83e5809bf8343f07f2f8330d22b813672d0d6b9e
YongXian Lam <yxlam@esrisingapore.com.sg>
2020-11-04 11:32:05 +0800

    Level 8. Dates and Times

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -2,28 +2,63 @@ package duke.task;
 
 import duke.command.DukeException;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline() {
-        this("", "");
+        this("");
+    }
+
+    public Deadline(String description) {
+        super(description);
+        this.by = LocalDateTime.now().plus(1, ChronoUnit.DAYS);
+        this.type = TaskType.DEADLINE;
     }
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.setBy(by);
         this.type = TaskType.DEADLINE;
+    }
+
+    public void setBy(LocalDateTime by) {
+        this.by = by;
+    }
+
+    public void setBy(String by) {
+
+        DateTimeFormatter formatter;
+
+        if (by.trim().toUpperCase().endsWith("PM") || by.trim().toUpperCase().endsWith("AM")) {
+            formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a");
+        } else {
+            formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        }
+
+        try {
+            this.by = LocalDateTime.parse(by, formatter);
+        } catch (DateTimeParseException ex) {
+            this.by = LocalDateTime.now();
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("d MMM yyyy hh:mm a")) + ")";
     }
 
     @Override
     public String toSavableString() {
-        return String.format("%s|%s", super.toSavableString(), by);
+        return String.format("%s|%s", super.toSavableString(), by.toEpochSecond(ZoneOffset.UTC));
     }
 
     @Override
@@ -35,6 +70,14 @@ public class Deadline extends Task {
             throw new DukeException("Rabak Sial, wrong data format for Deadline!", DukeException.DukeError.WRONG_DATA_FORMAT);
         }
 
-        this.by = arrString[3].trim();
+        try {
+
+            long epoch = Long.parseLong(arrString[3].trim());
+            this.by = LocalDateTime.ofEpochSecond(epoch, 0, ZoneOffset.UTC);
+
+        } catch (NumberFormatException | DateTimeException ex) {
+            throw new DukeException("Rabak Sial, wrong data format for Deadline! " + ex.getMessage(),
+                    DukeException.DukeError.WRONG_DATA_FORMAT);
+        }
     }
 }
--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -2,28 +2,62 @@ package duke.task;
 
 import duke.command.DukeException;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+
 public class Event extends Task {
 
-    protected String at;
+    protected LocalDateTime at;
 
     public Event() {
-        this("", "");
+        this("");
+    }
+
+    public Event(String description) {
+        super(description);
+        this.at  = LocalDateTime.now();
+        this.type = TaskType.EVENT;
     }
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.setAt(at);
         this.type = TaskType.EVENT;
+    }
+
+    public void setAt(LocalDateTime at) {
+        this.at = at;
+    }
+
+    public void setAt(String at) {
+
+        DateTimeFormatter formatter;
+
+        if (at.trim().endsWith("PM") || at.trim().endsWith("AM")) {
+            formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a");
+        } else {
+            formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        }
+
+        try {
+            this.at = LocalDateTime.parse(at, formatter);
+        } catch (DateTimeParseException ex) {
+            this.at = LocalDateTime.now();
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + at.format(DateTimeFormatter.ofPattern("d MMM yyyy hh:mm a")) + ")";
     }
 
     @Override
     public String toSavableString() {
-        return String.format("%s|%s", super.toSavableString(), at);
+        return String.format("%s|%s", super.toSavableString(), at.toEpochSecond(ZoneOffset.UTC));
     }
 
     @Override
@@ -35,6 +69,14 @@ public class Event extends Task {
             throw new DukeException("Rabak Sial, wrong data format for Event!", DukeException.DukeError.WRONG_DATA_FORMAT);
         }
 
-        this.at = arrString[3].trim();
+        try {
+
+            long epoch = Long.parseLong(arrString[3].trim());
+            this.at = LocalDateTime.ofEpochSecond(epoch, 0, ZoneOffset.UTC);
+
+        } catch (NumberFormatException | DateTimeException ex) {
+            throw new DukeException("Rabak Sial, wrong data format for Event! " + ex.getMessage(),
+                    DukeException.DukeError.WRONG_DATA_FORMAT);
+        }
     }
 }
